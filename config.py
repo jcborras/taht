fcf619a5fc715ca644f36c025b8304ed92593c99
Juan Carlos Borrás <jcborras@gmail.com>
2013-05-20 19:06:48 +0300

    Default settings don't look like a DDOS now

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ from random import randint
 
 Resource = namedtuple('Resource', ['name', 'url', 'polling_interval','check'])
 
-hs = Resource('HS', 'http://www.hs.fi', lambda: randint(1,3), lambda x: x.status_code==200)
+hs = Resource('HS', 'http://www.hs.fi', lambda: randint(10,30), lambda x: x.status_code==200)
 missing_resource = Resource('Missing Resource', 'http://fsdfsdfds.f-secure.com/', lambda: randint(2,4), lambda x: True)
-fb = Resource('Facebook', 'http://www.facebook.com', lambda: randint(2,4), lambda x: 'form id="login_form"' in x.text)
-rogueserver = Resource('RogueServer', 'http://localhost:5000', lambda: randint(0,1), lambda x: 'Expected' in x.text)
+fb = Resource('Facebook', 'http://www.facebook.com', lambda: randint(20,40), lambda x: 'form id="login_form"' in x.text)
+rogueserver = Resource('RogueServer', 'http://localhost:5000', lambda: randint(1,2), lambda x: 'Expected' in x.text)
 
 to_monitor = [rogueserver, hs, missing_resource, fb]
