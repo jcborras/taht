163a4a08fdbfdf1ffe89a15cb53193bf138dfcba
Juan Carlos Borrás <jcborras@gmail.com>
2013-05-20 18:57:32 +0300

    A rogue server generating errors and inconsistent content

---
+++ server.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+
+__author__ = 'jcb'
+
+from logging import basicConfig, DEBUG
+from random import sample
+
+from flask import abort, render_template, Flask
+
+APP_PORT = 5000
+APP_HOSTNAME = 'localhost'
+
+ROOT_URL='/'
+HTTP_STATUS_CODES = [200,400,200,404,200,500,200,501]
+
+app = Flask('RogueServer')
+formatstr = "%(asctime)s %(levelname)s %(message)s"
+basicConfig(format=formatstr, level=DEBUG)
+app.logger.setLevel(DEBUG)
+
+def random_content():
+    if sample([True,False],1)[0]:
+        return 'Expected'
+    return 'Unexpected'
+
+@app.route(ROOT_URL, methods=['GET'])
+def frontpage():
+    sc = sample(HTTP_STATUS_CODES,1)[0]
+    if sc==200:
+        return render_template("index.html", content=random_content())
+    abort(sc)
+
+if __name__ == '__main__':
+    app.run(host=APP_HOSTNAME, port=APP_PORT, debug=True)
