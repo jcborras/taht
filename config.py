2f40331078e3f09f59e13aac3ae85fdf561e7d69
Juan Carlos Borrás <jcborras@gmail.com>
2013-05-20 16:40:20 +0300

    Added a missing resource

--- config.py
+++ config.py
@@ -8,8 +8,9 @@ from random import randint
 Resource = namedtuple('Resource', ['name', 'url', 'polling_interval','check'])
 
 hs = Resource('HS', 'http://www.hs.fi', lambda: randint(0,0), lambda x: x.status_code==200)
+missing_resource = Resource('Missing Resource', 'http://fsdfsdfds.f-secure.com/', lambda: randint(0,0), lambda x: True)
 
-ToMonitor = [hs]
+ToMonitor = [hs, missing_resource]
 
 class TestDrive(TestCase):
     def setUp(self):
